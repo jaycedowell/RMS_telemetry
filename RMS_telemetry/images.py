aa85e55337e47c0e024c37f590e1480862f9927a
jaycedowell <jayce.dowell@gmail.com>
2025-08-11 10:01:15 -0600

    Clean up MIME type handling.

--- RMS_telemetry/images.py
+++ RMS_telemetry/images.py
@@ -1,5 +1,6 @@
 import os
 import glob
+import mimetype
 
 from .utils import get_archive_dir, get_frames_dir, timestamp_to_rfc2822, timed_lru_cache
 
@@ -74,22 +75,6 @@ def get_image(log_dir: str, date: Optional[str]=None) -> Optional[str]:
     return latest_image
 
 
-def _get_content_type(filename: str):
-    """
-    Helper function that takes in a filename and returns content type - limited
-    to PNGs, JPEGs, MP4s.  For all others 'application/octet-stream' is returned.
-    """
-    
-    if filename.endswith('.png'):
-        return 'image/png'
-    elif filename.endswith('.jpg') or filename.endswith('.jpeg'):
-        return 'image/jpeg'
-    elif filename.endswith('.mp4'):
-        return 'video/mp4'
-    else:
-        return 'application/octet-stream'
-
-
 @timed_lru_cache(seconds=300)
 def get_image_data(filename: str) -> Dict[str,Any]:
     """
@@ -100,7 +85,9 @@ def get_image_data(filename: str) -> Dict[str,Any]:
     
     data = {}
     if os.path.exists(filename):
-        data['content-type'] = _get_content_type(filename)
+        ct, en = mimetype.guess_file_type(filename)
+        data['content-type'] = ct
+        data['content-encoding'] = en
         data['last-modified'] = timestamp_to_rfc2822(os.path.getmtime(filename))
         with open(filename, 'rb') as fh:
             data['data'] = fh.read()
