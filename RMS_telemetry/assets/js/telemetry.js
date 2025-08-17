2e05a7333ff081c7fa4c5093008f99a17ad463c3
jaycedowell <jayce.dowell@gmail.com>
2025-08-17 09:16:32 -0600

    Was it the js all along?

--- RMS_telemetry/assets/js/telemetry.js
+++ RMS_telemetry/assets/js/telemetry.js
@@ -140,8 +140,8 @@ function updateHistory(response, status, xhr) {
     var date = response[idx];
     var date_str = date.slice(0,4) + "-" + date.slice(4,6) + "-" + date.slice(6,8);
     var entry = date_str;
-    entry += ' - <a href="/latest?date=' + date + '">Detailed Status</a>';
-    entry += ' - <a href="/latest/image?date=' + date + '">Stacked Meteors Image</a><br />';
+    entry += ' - <a href="/previous?date=' + date + '">Detailed Status</a>';
+    entry += ' - <a href="/previous/image?date=' + date + '">Stacked Meteors Image</a><br />';
     contents += entry;
   }
   
