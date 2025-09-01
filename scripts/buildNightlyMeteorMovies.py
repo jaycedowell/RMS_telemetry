212341d57760a482db53b1b647f33c69a2ff59cf
jaycedowell <jayce.dowell@gmail.com>
2025-09-01 09:43:27 -0600

    Add a rmsExternal script to automate converting FITS files into movies.

---
+++ scripts/buildNightlyMeteorMovies.py
@@ -0,0 +1,30 @@
+"""
+RMS-compatile external script for converting nightly FITS files into movies for
+RMS_telemetry.
+"""
+
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+import os
+import glob
+import time
+
+from RMS.Logger import getLogger
+
+from RMS_telemetry.images import fits_to_movie
+
+
+log = getLogger("logger")
+
+def rmsExternal(captured_night_dir, archived_night_dir, config):
+    fits_list = os.path.join(archived_night_dir, 'FF_*.fits')
+    for fitsname in fits_list:
+        t0 = time.time()
+        success = fits_to_movie(fitsname)
+        t1 = time.time()
+        
+        if success is not None:
+            log.debug(f"Converted {os.path.basename(fitsname)} to mp4 in {t1-t0:.1f} s")
+        else:
+            log.warning(f"Failed to convert {os.path.basename(fitsname)} to mp4")
