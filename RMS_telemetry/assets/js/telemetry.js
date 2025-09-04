8de764ca9fe4b68d916beca19e5119340221b9e6
jaycedowell <jayce.dowell@gmail.com>
2025-09-03 19:35:45 -0600

    Link to the details.

--- RMS_telemetry/assets/js/telemetry.js
+++ RMS_telemetry/assets/js/telemetry.js
@@ -153,6 +153,7 @@ function updateHistory(response, status, xhr) {
     var date_str = date.slice(0,4) + "-" + date.slice(4,6) + "-" + date.slice(6,8);
     var entry = date_str;
     entry += ' - <a href="/previous?date=' + date + '">Detailed Status</a>';
+    entry += ' - <a href="/previous/details.html?date=' + date + '">Meteor Details</a>';
     entry += ' - <a href="/previous/image?date=' + date + '">Stacked Meteors Image</a><br />';
     contents += entry;
   }
