0b8a37a1947a53903c3c98f0df39235ca007e59b
jaycedowell <jayce.dowell@gmail.com>
2025-09-06 07:57:57 -0600

    glob first

--- scripts/buildNightlyMeteorMovies.py
+++ scripts/buildNightlyMeteorMovies.py
@@ -18,7 +18,7 @@ from RMS_telemetry.images import fits_to_image, fits_to_movie
 log = getLogger("logger")
 
 def rmsExternal(captured_night_dir, archived_night_dir, config):
-    fits_list = os.path.join(archived_night_dir, 'FF_*.fits')
+    fits_list = glob.glob(os.path.join(archived_night_dir, 'FF_*.fits'))
     for fitsname in fits_list:
         t0 = time.time()
         success = fits_to_image(fitsname)
