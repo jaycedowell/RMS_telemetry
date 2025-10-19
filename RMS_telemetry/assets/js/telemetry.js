c4aec945ab56feacf868bc990701c01e9e8e6ea2
jaycedowell <jayce.dowell@gmail.com>
2025-10-19 11:38:51 -0600

    Ah, it's like a bad new line character.

--- RMS_telemetry/assets/js/telemetry.js
+++ RMS_telemetry/assets/js/telemetry.js
@@ -177,7 +177,7 @@ function fetchHistory() {
          });
 }
 
-const zhr_flux_re = /in the next 24 hrs<tspan x="\d+(\.\d+)?" dy="1em">​<\/tspan><tspan x="\d+(\.\d+)?" dy="1em">​<\/tspan><\/tspan>(?<zhr>\d+)<tspan x="\d+(\.\d+)?" dy="1em">​<\/tspan><tspan>meteors\/hr/;
+const zhr_flux_re = new RegExp(/in the next 24 hrs<tspan x="\d+(\.\d+)?" dy="1em">.*<\/tspan><tspan x="\d+(\.\d+)?" dy="1em">.*<\/tspan><\/tspan>(?<zhr>\d+)<tspan x="\d+(\.\d+)?" dy="1em">.*<\/tspan><tspan>meteors\/hr/, 'ms');
 const zhr_updated_re = new RegExp(/<p><b>Last update:\s*<\/b>.*<br>(?<date>\d+-\d+-\d+ \d+:\d+:\d+ UTC).*<br>Solar longitude (?<long>\d+(\.\d+)?) deg/, 'ms');
     
 function updateZHR(response, status, xhr) {
@@ -186,12 +186,12 @@ function updateZHR(response, status, xhr) {
     var mtch1 = response.match(zhr_flux_re);
     var mtch2 = response.match(zhr_updated_re);
     if( mtch1 != null && mtch2 != null ) {
-      sp.innerHTML = mtch1.groups['zhr'] + 'meteors/hr';
+      sp.innerHTML = mtch1.groups['zhr'] + ' meteors/hr';
       sp.classList.remove('loading');
       
       sp = document.getElementById('zhr_updated');
       if( sp != null ) {
-        sp.innerHTML = mtch2.groups('date');
+        sp.innerHTML = mtch2.groups['date'];
         sp.classList.remove('loading');
       }
     }
